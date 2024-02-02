94aea580fb2155027bf086f8aeceb839c8c69fc5
Gaurav <gaurav@iitmandi.ac.in>
2024-02-02 18:32:00 +0530

    Corrected an error in convergence check, added check for special cases.

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,4 +1,9 @@
 def sqrt2(x, debug=False):
+	from numpy import nan
+	if x==0:
+		return 0
+	elif x<0:
+		return nan
 	s=1.0
 	kmax=100
 	tol=1.0e-14
@@ -8,7 +13,7 @@ def sqrt2(x, debug=False):
 		s0 = s
 		s = 0.5 * (s + (x/s))
 		delta_s = s-s0
-		if(abs(delta_s)/x < tol):
+		if(abs(delta_s/x) < tol):
 			break
 	if debug:
 		print(f"Finally, the value of s={s:20.15f}")
