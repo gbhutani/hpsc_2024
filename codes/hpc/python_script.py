00600386945243addb4d23d284cc5e82ad2e15ef
Gaurav Bhutani <gaurav@iitmandi.ac.in>
2024-02-23 13:23:16 +0530

    python script for hpc

---
+++ codes/hpc/python_script.py
@@ -0,0 +1,9 @@
+import numpy as np
+# just to check if the virtual environment was enabled successfully or not
+a=2.0
+b=3.6
+print(f"a={a}, b={b}, a+b={a+b}")
+print(np.pi)
+#f = open("file11.txt", "a")
+#f.write(str(c))
+#f.close()
